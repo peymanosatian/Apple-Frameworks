546c421c123fca16e39e3ff63e3bc357a9a28c81
Peyman <peyman.osatian@gmail.com>
2024-03-31 12:50:23 +0200

    31 march

---
+++ Apple-Frameworks/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks
+//
+//  Created by Peyman Osatian on 2024-03-30.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
